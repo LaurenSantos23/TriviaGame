d3a1f04dc4d8d30775403ef60b04995fb559ec73
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2018-08-30 21:33:14 -0400

    cleaned up psuedo code in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,15 +6,28 @@
 $(document).ready(function() {
   var counter = 120
   var timer 
-  var myQuestions
+  var myQuestions = {
+    question0: "Trees",
+    question1: "Dogs", 
+    question2: "Coulrophobia",
+    question3: "Nudity",
+    question4: "Sigmund Freud",
+    question5: "Papaphobia", 
+    question7: "No Mobile Phone Coverage",
+    question8: "4 seconds",
+    question9: "Beards",
+  }  
   var correct = 0
   var incorrect = 0
+//Have start button begin countdown (120 seconds) on click function (document.onkeyup)reference function
+
+// Player has 120 seconds to play the game
 
   function countDown() {
    // counter = counter -1;//
     counter--;
     $("#counterNumber").html(counter);
-  // Your code here
+ 
 }
 
 
@@ -27,44 +40,20 @@ $(document).ready(function() {
       clearInterval(timer)
       var question0Answer = $("input[name='question-0']:checked").val()
       myQuestions.question0.correctAnswer//
-      if question0Answer === myQuestions.question0.correctAnswer //this means answer is right, increase correct score or incorrect score ++
+      if (question0Answer === myQuestions.question0) {//this means answer is right, increase correct score or incorrect score ++
 
         console.log(question0Answer)
+      }
     })    ;
   });
 
 
-//Have start button begin countdown (120 seconds) on click function (document.onkeyup)reference function
-
-//Make variables for questions, answers and indicate correct answer/ objects= key/value pairs
- myQuestions = {question0: {correctAnswer: "Trees"},
-                question1: {correctAnswer: "Dogs"}, 
-                question2: {correctAnswer: "Coulrophobia"},
-                question3: {correctAnswer:"Nudity"},
-                question4: {correctAnswer:"Sigmund Freud"},
-                question5: {correctAnswer:"Parents-in-laws"},
-                question6: {correctAnswer:"Papaphobia"}, 
-                question7: {correctAnswer: "No Mobile Phone Coverage"},
-                question8: {correctAnswer: "4 seconds"},
-                question9: {correctAnswer: "Beards"},
-              }  
-
-
-
-
 //Keep track of number of correct/incorrect answers for player
 
-// Player has 120 seconds to play the game
 
 //Show player how many correct/incorrect answers they had
 
 
-
-
-
-//$("h2").on("click", function(event){
-    
-//})
 
 
 
